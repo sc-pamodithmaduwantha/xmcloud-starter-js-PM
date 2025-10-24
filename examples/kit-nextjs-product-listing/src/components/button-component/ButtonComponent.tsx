3834b5ea5c499b7227f3a7d582c87a82968573eb
Pamodith Maduwantha <pamo@sitecore.net>
2025-10-24 16:19:57 +0530

    Unit test (#35)
    * Added Jest configuration, setup, and component tests with mocks to ensure robust handling of optional fields and edge cases in the kit-nextjs-skate-park example.
    
    * Update package.json for kit-nextjs-article-starter: rename application, enhance dependencies, and refine scripts for improved development and testing experience.
    
    * Dmz two test (#30)
    
    * test (#24)
    
    Co-authored-by: Pamodith Maduwantha <pamodith.maduwantha+sitecore@sitecore.com>
    
    * Dmz one test (#25)
    
    * test
    
    * clg
    
    ---------
    
    Co-authored-by: Pamodith Maduwantha <pamodith.maduwantha+sitecore@sitecore.com>
    
    * unused imports
    
    * debugger
    
    * test
    
    * debugger
    
    * test
    
    ---------
    
    Co-authored-by: Pamodith Maduwantha <pamodith.maduwantha+sitecore@sitecore.com>
    
    * pretteir fix
    
    * type error fix
    
    * fix
    
    * fix-new
    
    ---------
    
    Co-authored-by: Pamodith Maduwantha <pamodith.maduwantha+sitecore@sitecore.com>

--- examples/kit-nextjs-product-listing/src/components/button-component/ButtonComponent.tsx
+++ examples/kit-nextjs-product-listing/src/components/button-component/ButtonComponent.tsx
@@ -12,7 +12,7 @@ import { Default as ImageWrapper } from '@/components/image/ImageWrapper.dev';
 import { ImageField } from '@sitecore-content-sdk/nextjs';
 import { ButtonVariants, ButtonSize } from '@/enumerations/ButtonStyle.enum';
 /**
- * Model used for Sitecore Component integration
+ * Model used for Sitecore Component integration.
  */
 
 export type ButtonFields = {
@@ -47,7 +47,7 @@ const isValidEditableLink = (link: LinkField, icon?: ImageField) => {
       link?.value?.href !== 'http://')
   );
 };
-
+debugger;
 export type ButtonComponentProps = ComponentProps & ButtonFields;
 const ButtonBase = (
   props: ButtonFields['params'] &
@@ -69,6 +69,11 @@ const ButtonBase = (
   const ariaHidden = typeof isAriaHidden === 'boolean' ? isAriaHidden : true;
   const iconName = icon?.value as EnumValues<typeof IconName>;
   if (!isPageEditing && !linkIsValid(buttonLink)) return null;
+  console.log(
+    '%cexamples\kit-nextjs-product-listing\src\components\button-component\ButtonComponent.tsx:72 objectobject',
+    'color: #007acc;',
+    iconName
+  );
 
   return (
     <Button asChild variant={variant} size={size} className={className}>
